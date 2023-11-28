080597074b9963afe7b9af5931285df4fd11ad22
david397 <ologunlekodavid@gmail.com>
2023-11-28 20:47:09 +0100

    100 u p e

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 capitalize = False
 for i in range(122, 96, -1):
-    if not capitalize:
-        print(chr(i), end="")
-        capitalize = True
-    else:
-        print(chr(i).upper(), end="")
-        capitalize = False
+    print("{}".format(chr(i).upper() if capitalize else chr(i)), end="")
+    capitalize = not capitalize
