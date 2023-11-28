60dd02e7898326353e42fb7fc8e86877ae668f96
david397 <ologunlekodavid@gmail.com>
2023-11-28 19:15:25 +0100

    no 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 10):
     for e in range(0, 10):
         if i < e:
             if i == 8 and e == 9:
-                print(f"{i}{e}")
+                print("{}".format(i) + "{}".format(e))
             else:
-                print(f"{i}{e}, ", end="")
+                print("{}, ".format(i) + "{}".format(e), end="")
