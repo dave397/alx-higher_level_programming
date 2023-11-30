0044add5e606d39be11472e300844be6e8754a52
david397 <ologunlekodavid@gmail.com>
2023-11-30 22:53:27 +0100

    rm space

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         print("{}".format(arg_len) + " argument:")
     if arg_len > 1:
         print("{}".format(arg_len) + " arguments:")
-        
+
     if arg_len >= 1:
         for i in range(1, arg_len+1):
             print("{}: {}".format(i, sys.argv[i]))
