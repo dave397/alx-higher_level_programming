463298cf57676888eda454338e9c2972bb6f41b6
david397 <ologunlekodavid@gmail.com>
2023-12-02 18:00:19 +0100

    eplace num in array

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def num_len(my_list=[]):
+    count = 0
+    for num in my_list:
+        count += 1
+    return count
+
+
+def replace_in_list(my_list, idx, element):
+    count = num_len(my_list)
+
+    if idx < 0 | idx > count - 1:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
